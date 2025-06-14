1f1cd32a1e491860193491c48ff8cd8be0f3c518
Vaishnavi-Kumari27 <vaishnaavi29.15@gmail.com>
2025-06-14 20:28:15 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -15,4 +15,32 @@ WARNING:
 USE master;
 GO
 
+-- Drop and recreate the 'DaraWarehouse' database 
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN 
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Datawarehouse;
+END;
+GO 
+
+
+-- Create the 'DataWarehouse' database 
+CREATE DATABASE DataWarehouse;
+GO 
+
+USE DataWarehouse;
+GO
+
+
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO 
+
+CREATE SCHEMA gold;
+GO 
+
 
